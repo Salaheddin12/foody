bbea8319c08a5c9d7812e3a2bb4cb0559ce4e700
SalahEddine <ab.salah012@gmail.com>
2021-01-12 22:31:52 +0100

    removed unused code/files

--- components/Search/index.js
+++ components/Search/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './Search.module.css'
+import {useState} from 'react'
 
 export default () => {
+    const [Query, setQuery] = useState('');
+
+    const onQueryChange = (query)=>{
+        setQuery(query)
+    }
     return (
         <div className={styles.Wrapper}>
-            <input className={styles.search}/>
-            <a className={styles.button} href="/search">Search</a>
+            <input className={styles.search} onChange={(e)=>onQueryChange(e.currentTarget.value)}/>
+            <a className={styles.button} href={`/search/${Query}`}>Search</a>
         </div>
     );
 };
