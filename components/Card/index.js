cb640e7c84cf3b8a1a8d1c91e701a9484782d9a9
SalahEddine <ab.salah012@gmail.com>
2021-01-12 22:07:02 +0100

    updated Card styles

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,17 +1,25 @@
 import styles from './Card.module.css'
 import Image from 'next/image'
 
-export default  () => {
+const queryString = require('query-string');
+
+export default  ({recipe,query}) => {
+    const {calories,image,label,dietLabels,healthLabels,ingredients} = recipe;
+    const labels =dietLabels.concat(healthLabels);
+    const data = {
+        selected:recipe.uri,
+        query};
+
     return (
-        <div className={styles.Wrapper}>
-          <Image style={{borderRadius:'50%'}} src="/dish.jpg" width={150} height={150} layout='fixed'/> 
-          <h2 className={styles.Label}>Chicken Vesuvio</h2>
-          <h3 className={styles.DietLabel}>Low-Carb</h3>
+        <a className={styles.Wrapper} href={`/Recipes/${queryString.stringify(data)}`}>
+          <Image src={image} width={150} height={150} layout='fixed'/> 
+          <h2 className={styles.Label}>{label||''}</h2>
+          <h3 className={styles.Labels}>{labels.join(', ')}</h3>
           <div className={styles.Description}>
-              <h2>121 Calories</h2>
-              <h2>3 Ingredients</h2>
+              <h2>{Math.floor(calories)} Calories</h2>
+              <h2>{ingredients.length} Ingredients</h2>
           </div>
-        </div>
+        </a>
     );
 };
 
